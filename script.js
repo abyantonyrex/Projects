bcff86b23e999efceb3eab353e438a3929d855e6
abyantonyrex <131639533+abyantonyrex@users.noreply.github.com>
2025-03-12 13:24:01 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("resume-btn").addEventListener("click", function() {
-    const link = document.createElement("a");
-    link.href = "https://drive.google.com/uc?export=download&id=1UTNjsSezF7yvTAKSDlYNKmz9py-JIW5M";
-    link.download = "aby_resume.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-});
